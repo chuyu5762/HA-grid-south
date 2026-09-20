31433c4c8c37cd1795f98847b2aafd5beb13a01f
chuyu5762 <663347476@qq.com>
2026-09-21 03:43:43 +0800

    chore: drop unused HomeAssistantError import in sensor.py

--- custom_components/china_southern_power_grid_stat/sensor.py
+++ custom_components/china_southern_power_grid_stat/sensor.py
@@ -19,7 +19,6 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_USERNAME, STATE_UNAVAILABLE, UnitOfEnergy
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import (
